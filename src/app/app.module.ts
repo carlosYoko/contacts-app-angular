839b17721c113ef9c5e8a7cdddd83dec847cfd31
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-10-01 19:50:44 +0200

    created structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddEditContactComponent } from './components/add-edit-contact/add-edit-contact.component';
+import { ListContactComponent } from './components/list-contact/list-contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ConfirmationMessageComponent } from './components/shared/confirmation-message/confirmation-message.component';
+
+import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditContactComponent,
+    ListContactComponent,
+    NavbarComponent,
+    ConfirmationMessageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/shared/confirmation-message/confirmation-message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-message',
+  templateUrl: './confirmation-message.component.html',
+  styleUrls: ['./confirmation-message.component.css']
+})
+export class ConfirmationMessageComponent {
+
+}
