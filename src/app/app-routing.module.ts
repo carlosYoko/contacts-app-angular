b637fb396d4ff8833507fe06ac35c34d42b09db5
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-10-12 19:43:41 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditContactComponent } from './components/add-edit-contact/add-edit-contact.component';
+import { ListContactComponent } from './components/list-contact/list-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListContactComponent },
+  { path: 'add', component: AddEditContactComponent },
+  { path: 'edit/:id', component: AddEditContactComponent },
+  { path: '**', component: ListContactComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
